8d24b2fe54e6edead7076209454f9a6e8ee72cd4
Layla Oh <crispylemon7120@gmail.com>
2025-10-20 11:17:21 +0900

    feat(calculator): 입력값이 빈 문자열인 경우 0을 반환하도록 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Console.readLine();
+
+        if(input.isBlank()) {
+            System.out.println("결과 : 0");
+            return;
+        }
+
         String target = input;
 
         String separator = "[,:]";
