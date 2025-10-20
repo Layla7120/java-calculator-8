b937d124876008c4719f647c1425beae074f53a3
Layla Oh <crispylemon7120@gmail.com>
2025-10-20 10:48:55 +0900

    feat(parser): 커스텀 및 기본 구분자 처리 로직 구현
    - 문자열 앞부분의 '//'와 '\n' 사이 문자를 커스텀 구분자로 인식
    - 커스텀 구분자가 없는 경우 기본 구분자(‘,’, ‘:’) 사용
    - 예: "//;\n1;2;3" → 6

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,47 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+        String target = input;
+
+        String separator = "[,:]";
+
+        String custom_separator = getCustomSeparator(input);
+
+        if(!custom_separator.isBlank()) {
+            separator = separator + "|" + Pattern.quote(custom_separator);
+
+            int separatorIndex = input.indexOf("\\n");
+            target = input.substring(separatorIndex + 2);
+
+        }
+
+        String[] numbers = target.split(separator);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    private static String getCustomSeparator(String input) {
+        if(!input.startsWith("//")){
+            return "";
+        }
+
+        int endIndex = input.indexOf("\\n");
+
+        System.out.println("입력 문자열 길이: " + input.length());
+        System.out.println("인덱스 3의 문자: " + input.charAt(3));
+
+        if(endIndex <= 2){
+            return "";
+        }
+
+        return input.substring(2, endIndex);
     }
 }
